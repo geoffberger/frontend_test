4b91742352af3e01e2fa24af52a53b1511fc9777
Geoff Berger <gberger@zappos.com>
2013-07-28 18:43:09 -0400

    added smaller styling and more templates to text-search

--- js/text-search.js
+++ js/text-search.js
@@ -1,7 +1,6 @@
 /* globals Util, Finder */
 (function() {
   var el = document.getElementById('search_input'),
-      template = document.getElementById('results-template').innerHTML,
       contents = document.getElementById('search_text');
 
   Util.addEvent(document.getElementById('search-form'), 'submit', function(e) {
@@ -11,8 +10,22 @@
     phrase = document.getElementById('search_input').value;
 
     finder = new Finder(contents, phrase, function(occurrences) {
-      var results = Util.template(template, {
+      var count, results, template;
+
+      count = phrase.split(' ').length;
+
+      if (occurrences) {
+        template = document.getElementById('results-template').innerHTML;
+      } else if (!phrase) {
+        template = document.getElementById('no-phrase-template').innerHTML;
+      } else {
+        template = document.getElementById('no-results-template').innerHTML;
+      }
+
+      results = Util.template(template, {
         occurrences: occurrences,
+        context: 'occurence' + ((occurrences !== 1) ? 's' : ''),
+        wordCount: 'word' + ((count > 1) ? 's' : ''),
         phrase: phrase
       });
 
