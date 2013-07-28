17c4de43f8163d23bb97e8a92dbdfe19cd833f31
Geoff Berger <gberger@zappos.com>
2013-07-28 15:57:17 -0400

    added commenting to existing code to the tabbed_content page

--- js/tabbed-content.js
+++ js/tabbed-content.js
@@ -1,4 +1,7 @@
 (function() {
+  // This is used for the first version or the "container-original". This
+  // version roughly uses the same elements that came with the problem and I
+  // thought it would be neat to create a class around this.
   var container = document.getElementById('container-original'),
   headings = container.getElementsByTagName('h3'),
   contents = container.getElementsByTagName('p');
@@ -7,6 +10,11 @@
 }());
 
 (function() {
+  // This represents how I would create the HTML for the problem. I took the
+  // approach of having the design represent the content. With JS, you have a
+  // great detail of flexibility for moving things around. However, I felt as
+  // though the markup should reflect the content, so this class reflects the
+  // markup produced.
   new Tabtastic(document.getElementById('container-modified'));
 }());
 
